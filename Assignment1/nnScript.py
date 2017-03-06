735ed6a7c2e9ffaceb18521b3da6cae50491bcd8
Benjamin Riexinger <bpriexin@buffalo.edu>
2017-03-06 16:53:04 -0500

    spelling corrections

--- Assignment1/nnScript.py
+++ Assignment1/nnScript.py
@@ -248,8 +248,7 @@ def nnPredict(w1, w2, data):
     % label: a column vector of predicted labels"""
 
     labels = np.array([])
-   
-    labels = np.array([])
+
     "%Create Bias for Data"
     bias = np.ones(len(data),1)
     "%Add Bias to Data"
@@ -270,7 +269,7 @@ def nnPredict(w1, w2, data):
     ol = sigmoid(bl)
     
     print("BL is: " + bl)
-    print("ol is: " + ol)
+    print("OL is: " + ol)
     
     #still need to predict labels
 
